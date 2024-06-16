64e2a4b5552da1cf829dfe587b4be21ae0fa9ebd
nikhilNathwani <nnathwani36@gmail.com>
2024-06-16 13:32:26 -0400

    commiting responsive filter code

---
+++ responsive.js
@@ -0,0 +1,117 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/*                                                          */
+/*     VARIABLES                                            */
+/*                                                          */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+// Breakpoint
+const minWidthAdjacentMode = 1000; //px
+
+// App states
+const APP_STATE = {
+	INITIAL: "initialMode",
+	STACKED: "stackedMode",
+	ADJACENT: "adjacentMode",
+};
+
+// Page sections
+var appContainer = document.getElementById("app");
+var filterContainer = document.getElementById("filter_container");
+var datavisContainer = document.getElementById("datavis_container");
+
+// Buttons
+const submitButton = document.getElementById("submit-button");
+const filterReturnButton = document.getElementById("return-to-filters");
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/*                                                          */
+/*     UI TWEAK                                             */
+/*                                                          */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+// Shift filter container in initial state to account for scrollbar width when centering it
+const scrollbarWidth = appContainer.offsetWidth - appContainer.clientWidth;
+var left = parseFloat(
+	window.getComputedStyle(filterContainer).getPropertyValue("left")
+);
+filterContainer.style.left = left - scrollbarWidth / 2 + "px";
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/*                                                          */
+/*     EVENT HANDLING                                         */
+/*                                                          */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+// Handle submit
+submitButton.addEventListener("click", function () {
+	// Undo the UI tweak (adjusting css left property of filterContainer in initial state)
+	filterContainer.style.left = "0px";
+
+	if (window.innerWidth < minWidthAdjacentMode) {
+		setAppState(APP_STATE.STACKED);
+		datavisContainer.scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+		});
+	} else {
+		setAppState(APP_STATE.ADJACENT);
+		datavisContainer.scrollTo({ top: 0, behavior: "smooth" });
+	}
+});
+
+// Handle screen resize
+window.addEventListener("resize", function () {
+	if (appContainer.classList.contains(APP_STATE.INITIAL)) {
+		return;
+	}
+	if (
+		window.innerWidth < minWidthAdjacentMode &&
+		!appContainer.classList.contains(APP_STATE.STACKED)
+	) {
+		setAppState(APP_STATE.STACKED);
+		datavisContainer.scrollIntoView({ block: "start" });
+		return;
+	}
+	if (
+		window.innerWidth >= minWidthAdjacentMode &&
+		!appContainer.classList.contains(APP_STATE.ADJACENT)
+	) {
+		setAppState(APP_STATE.ADJACENT);
+	}
+});
+
+// Handle 'return to filters' button (only shown in stacked mode)
+filterReturnButton.addEventListener("click", function () {
+	filterContainer.scrollIntoView({ behavior: "smooth", block: "start" });
+});
+
+// Show 'return to filters' button (when filters are out of view)
+const observer = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			if (!entry.isIntersecting) {
+				// Element is out of view
+				filterReturnButton.style.display = "block";
+			} else {
+				// Element is in view
+				filterReturnButton.style.display = "none";
+			}
+		});
+	},
+	{
+		threshold: 0.5, // Trigger when 50% of the element is out of view
+		rootMargin: "-50px 0px 0px 0px", // Adjust this value as needed
+	}
+);
+observer.observe(filterContainer);
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/*                                                          */
+/*     HELPER FUNCTIONS                                        */
+/*                                                          */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+function setAppState(state) {
+	Object.values(APP_STATE).forEach((state) =>
+		appContainer.classList.remove(state)
+	); // Remove all possible states
+	appContainer.classList.add(state); // Add the desired state
+}
